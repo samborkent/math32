b1dcb953e2983dd447546b0a4e33a80c810b4ecc
Sam Borkent <sam_borkent@hotmail.com>
2025-03-23 12:07:38 +0100

    feat: added roundtoeven implementation

--- round.go
+++ round.go
@@ -8,6 +8,8 @@ package math32
 
 import "math"
 
+const halfMinusULP = (1 << (shift - 1)) - 1
+
 // Round returns the nearest integer, rounding half away from zero.
 //
 // Special cases are:
@@ -42,6 +44,46 @@ func Round(x float32) float32 {
 		e -= bias
 		bits += half >> e
 		bits &^= fracMask >> e
+	}
+
+	return math.Float32frombits(bits)
+}
+
+// RoundToEven returns the nearest integer, rounding ties to even.
+//
+// Special cases are:
+//
+//	RoundToEven(±0) = ±0
+//	RoundToEven(±Inf) = ±Inf
+//	RoundToEven(NaN) = NaN
+func RoundToEven(x float32) float32 {
+	// RoundToEven is a faster implementation of:
+	//
+	// func RoundToEven(x float32) float32 {
+	//   t := math32.Trunc(x)
+	//   odd := math32.Remainder(t, 2) != 0
+	//   if d := math32.Abs(x - t); d > 0.5 || (d == 0.5 && odd) {
+	//     return t + math32.Copysign(1, x)
+	//   }
+	//   return t
+	// }
+	bits := math.Float32bits(x)
+	e := uint32(bits>>shift) & mask
+
+	if e >= bias {
+		// Round abs(x) >= 1.
+		// - Large numbers without fractional components, infinity, and NaN are unchanged.
+		// - Add 0.499.. or 0.5 before truncating depending on whether the truncated
+		//   number is even or odd (respectively).
+		e -= bias
+		bits += (halfMinusULP + (bits>>(shift-e))&1) >> e
+		bits &^= fracMask >> e
+	} else if e == bias-1 && bits&fracMask != 0 {
+		// Round 0.5 < abs(x) < 1.
+		bits = bits&signMask | uvone // +-1
+	} else {
+		// Round abs(x) <= 0.5 including denormals.
+		bits &= signMask // +-0
 	}
 
 	return math.Float32frombits(bits)
--- round_test.go
+++ round_test.go
@@ -50,3 +50,35 @@ func TestRound(t *testing.T) {
 		}
 	}
 }
+
+var vfroundEvenSC = [][2]float32{
+	{0, 0},
+	{1.39067116e-39, 0}, // denormal
+	{0.49999994, 0},     // 0.5-epsilon
+	{0.5, 0},
+	{0.5000001, 1}, // 0.5+epsilon
+	{-1.5, -2},
+	{-2.5, -2},
+	{NaN(), NaN()},
+	{Inf(1), Inf(1)},
+	{22515249.5, 22515250}, // 1 bit fraction
+	{22515250.5, 22515250},
+	{45030495.5, 45030496}, // 1 bit fraction, rounding to 0 bit fraction
+	{45030497, 45030497},   // large integer
+}
+
+func TestRoundToEven(t *testing.T) {
+	t.Parallel()
+
+	for i := range len(vf) {
+		if f := RoundToEven(vf[i]); !alike(round[i], f) {
+			t.Errorf("RoundToEven(%g) = %g, want %g", vf[i], f, round[i])
+		}
+	}
+
+	for i := range len(vfroundEvenSC) {
+		if f := RoundToEven(vfroundEvenSC[i][0]); !alike(vfroundEvenSC[i][1], f) {
+			t.Errorf("RoundToEven(%g) = %g, want %g", vfroundEvenSC[i][0], f, vfroundEvenSC[i][1])
+		}
+	}
+}
